7133963b1d5bb681c1b46f8cbf0237625373524e
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-05-25 17:24:32 +0000

    Include standalone test compile/run script in new namespace (depends on my path)

---
+++ standalone-test.sh
@@ -0,0 +1,2 @@
+gcc -Iinclude -I/home/guest/panloader/trans trans-builder.c pandev.c trans-test.c allocate.c assemble.c limare-swizzle.c slow-framebuffer.c -lX11 -lm -o standalone-test
+./standalone-test
