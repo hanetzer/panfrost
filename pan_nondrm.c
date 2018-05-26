c6bb6acaeb4fb04dbfc9b35734b87fe30f87d6be
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-05-26 20:26:35 +0000

    Cleanup nondrm.c

--- pan_nondrm.c
+++ pan_nondrm.c
@@ -24,7 +24,6 @@
 /* From the kernel module */
 
 #define MALI_MEM_MAP_TRACKING_HANDLE (3ull << 12)
-#define MALI_CONTEXT_CREATE_FLAG_NONE 0
 
 int
 pandev_ioctl(int fd, unsigned long request, void *args)
@@ -55,45 +54,16 @@ pandev_standard_allocate(int fd, int va_pages, int flags, u64 *out)
 	return pandev_general_allocate(fd, va_pages, va_pages, 0, flags, out);
 }
 
-/**
- * Low-level open call, used by the main pandev_open
- */
-
-int
-pandev_raw_open()
-{
-	return open("/dev/mali0", O_RDWR | O_NONBLOCK | O_CLOEXEC);
-}
-
-/* The Memmap Tracking Handle needs to mapped for vendor kernel; it serves no
- * real purpose */
-
-u8*
-pandev_map_mtp(int fd)
-{
-	return mmap(NULL, 4096, PROT_NONE, MAP_SHARED, fd, MALI_MEM_MAP_TRACKING_HANDLE);
-}
-
-/**
- * Open the device file for communicating with the vendor kernelspace driver,
- */
-
 int
 pandev_open()
 {
-	int fd = pandev_raw_open();
+	struct mali_ioctl_get_version version = { .major = 10, .minor = 4 };
+	struct mali_ioctl_set_flags args = {};
 
-	struct mali_ioctl_get_version version = {
-		.major = 10,
-		.minor = 4
-	};
-
-	struct mali_ioctl_set_flags args = {
-		.create_flags = MALI_CONTEXT_CREATE_FLAG_NONE
-	};
+	int fd = open("/dev/mali0", O_RDWR | O_NONBLOCK | O_CLOEXEC);
 
 	assert(pandev_ioctl(fd, MALI_IOCTL_GET_VERSION, &version) == 0);
-	assert(pandev_map_mtp(fd) != MAP_FAILED);
+	assert(mmap(NULL, 4096, PROT_NONE, MAP_SHARED, fd, MALI_MEM_MAP_TRACKING_HANDLE) != MAP_FAILED);
 	assert(pandev_ioctl(fd, MALI_IOCTL_SET_FLAGS, &args) == 0);
 
 	return fd;
