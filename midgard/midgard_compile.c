b14c60127fdcc11e2df1bddd409f9bd533642580
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-06-02 22:33:07 +0000

    Enable registers in bcsel

--- midgard/midgard_compile.c
+++ midgard/midgard_compile.c
@@ -576,8 +576,7 @@ emit_alu(compiler_context *ctx, nir_alu_instr *instr)
 			 * shifted over one from NIR */
 
 			/* XXX: Force component correct */
-			assert (is_ssa);
-			int condition = instr->src[0].src.ssa->index;
+			int condition = nir_src_index(instr->src[0].src);
 
 			const midgard_vector_alu_src alu_src = {
 				.swizzle = SWIZZLE(COMPONENT_X, COMPONENT_X, COMPONENT_X, COMPONENT_X),
