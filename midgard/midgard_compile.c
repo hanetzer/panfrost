0f880b5690bed353acc7df7f9b921198e2786c2c
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-09-07 19:28:32 -0700

    midgard: Disable csel optimization so I can work on branching

--- midgard/midgard_compile.c
+++ midgard/midgard_compile.c
@@ -413,7 +413,7 @@ optimise_nir(nir_shader *nir)
 		NIR_PASS(progress, nir, nir_opt_dce);
 		NIR_PASS(progress, nir, nir_opt_dead_cf);
 		NIR_PASS(progress, nir, nir_opt_cse);
-		NIR_PASS(progress, nir, nir_opt_peephole_select, 8);
+		//NIR_PASS(progress, nir, nir_opt_peephole_select, 8);
 		NIR_PASS(progress, nir, nir_opt_algebraic);
 		NIR_PASS(progress, nir, nir_opt_constant_folding);
 		NIR_PASS(progress, nir, nir_opt_undef);
